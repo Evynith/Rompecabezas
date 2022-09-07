c81828af6c404ce00545964d640737d7fabc5f0f
Evynith <evynith@gmail.com>
2022-09-07 19:26:20 -0300

    drag and drop de elementos

--- funcionalidad.js
+++ funcionalidad.js
@@ -22,6 +22,43 @@ class Pieza {
     setLadoDer (lado) {this.ladoDer = lado}
     setLadoInf (lado) {this.ladoInf = lado}
     setLadoSup (lado) {this.ladoSup = lado}
+
+    setX (initX) {
+        if (initX<0){
+            this.datosPieza.posXInit = 0
+        } else if (initX > canvas.width- this.datosPieza.tamanio){
+            this.datosPieza.posXInit = canvas.width- this.datosPieza.tamanio
+        } else {
+            this.datosPieza.posXInit = initX
+        }
+    }
+    setY (initY) {
+        if (initY<0){
+            this.datosPieza.posYInit = 0
+        }else if (initY > canvas.height - this.datosPieza.tamanio){
+            this.datosPieza.posYInit = canvas.height - this.datosPieza.tamanio
+        } else {
+            this.datosPieza.posYInit = initY
+        }
+    }
+    getTam() {
+        return this.datosPieza.tamanio;
+    }
+
+    getDif (mouseX, mouseY) {
+        let x = mouseX - this.datosPieza.posXInit
+        let y = mouseY - this.datosPieza.posYInit
+        return {"x": x, "y":y}
+    }
+
+    isElem(posicion) {
+        if (posicion.x > this.datosPieza.posXInit && posicion.x < this.datosPieza.posXInit + this.datosPieza.tamanio) {
+            if  (posicion.y > this.datosPieza.posYInit && posicion.y < this.datosPieza.posYInit + this.datosPieza.tamanio) {
+                return true
+            }
+        }
+        return false
+    }
    
     draw (){
         ctx.drawImage(this.datosImg.img,this.datosImg.dx, this.datosImg.dy, this.datosImg.tamanioImg, this.datosImg.tamanioImg, 
@@ -57,9 +94,9 @@ function inicializacionPiezas(pxs, img) {
         piezas[i]=new Array(limiteAlto)
         for (y=0, j=0, dy=0; y<pxs.height, j<limiteAlto; y+= divisor, j++, dy+=divisorImg) {
 
-            let datosPieza = {"posXInit":x,"posXFinal": x+divisor,"posYInit": y, "posYFinal": y+divisor, "tamanio": divisor}
+            let datosInitPieza = {"posXInit":x,"posYInit": y, "tamanio": divisor}
             let datosImg = {"img":img, "dx":dx, "dy": dy, "tamanioImg":divisorImg}
-            piezas[i][j] = new Pieza(datosPieza, datosImg);
+            piezas[i][j] = new Pieza(datosInitPieza, datosImg);
             if (i>0) {
                 let lado = this.randomCeroUno();
                 piezas[i][j].setLadoIzq(lado);
@@ -81,15 +118,22 @@ function calcularTamañoNuevo(img)  {
     return {"height": newHeight, "width": newWidth}
 };
 
-function dibujarImagen(img, newWidth, newHeight){
-    ctx.drawImage(img, 0, 0, newWidth, newHeight);
-};
-
 function dibujarPiezas() {
     for (i=0; i< piezas.length; i++){
         for (j=0; j<piezas[i].length; j++) {
             let pieza = piezas[i][j]
             pieza.draw()
+        }
+    }
+}
+
+function barajar() {
+    for (i=0; i< piezas.length; i++){
+        for (j=0; j<piezas[i].length; j++) {
+            let pieza = piezas[i][j]
+            let tam = pieza.getTam()
+            pieza.setX(Math.random() * (canvas.width - tam +1) + 0);
+            pieza.setY(Math.random() * (canvas.height - tam +1) + 0);
         }
     }
 }
@@ -108,9 +152,44 @@ input.onchange = () => {
             nvoTamaño = calcularTamañoNuevo(img)
             document.body.appendChild(img);
             inicializacionPiezas( nvoTamaño, img)
-            // dibujarImagen(img,canvas.width,canvas.height)
+            barajar()
             dibujarPiezas()
 
         };
     };
 };
+
+function limpiarCanvas() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+}
+
+let press = false
+let elem 
+let dif = 0
+canvas.onmousedown = (e) => {
+    press = true
+    for (i=0; i< piezas.length; i++){
+        for (j=0; j<piezas[i].length; j++) {
+            let pieza = piezas[i][j]
+            let es = pieza.isElem({"x": e.layerX,"y":e.layerY})
+            if (es) {
+                elem = pieza
+            }
+        }
+    }
+    if (elem != undefined && press){
+        dif = elem.getDif(e.layerX, e.layerY)
+    }
+}
+canvas.onmousemove = (e) => {
+    if (elem != undefined && press){
+        elem.setX(e.layerX - dif.x)
+        elem.setY(e.layerY - dif.y)
+        limpiarCanvas()
+        dibujarPiezas()
+    }
+}
+canvas.onmouseup = () => {
+    press = false;
+    dif = 0
+}
